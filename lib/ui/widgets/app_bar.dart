1a678120730ebe2c88c05dab6b5b4002da3e399b
chat screen <sarahqueen103@gmail.com>
2024-11-29 15:23:49 +0200

    add asstets , splash screen ,home screen

---
+++ lib/ui/widgets/app_bar.dart
@@ -0,0 +1,11 @@
+abstract class AppAssets {
+  static const base = "assets/images/";
+  static const String splash = "${base}splash.png";
+  static const String icRadio = "${base}ic_radio.png";
+  static const String icAhadeth = "${base}ic_ahadeth.png";
+  static const String icQuran = "${base}ic_quran.png";
+  static const String icSebha = "${base}ic_sebha.png";
+  static const String quranTabLogo = "${base}quran_tab_logo.png";
+  static const String ahadethTabLogo = "${base}ahadeth_tab_logo.png";
+  static const String mainBackground = "${base}main_background.png";
+}
